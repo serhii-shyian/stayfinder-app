4f6229db60463c23949c8f412aa522530766e79a
Serhii Shyian <serhii.shn@gmail.com>
2025-03-06 11:35:21 +0200

    add user-manage-feature

--- src/main/java/com/example/stayfinder/mapper/UserMapper.java
+++ src/main/java/com/example/stayfinder/mapper/UserMapper.java
@@ -3,12 +3,28 @@ package com.example.stayfinder.mapper;
 import com.example.stayfinder.config.MapperConfig;
 import com.example.stayfinder.dto.user.UserRegisterRequestDto;
 import com.example.stayfinder.dto.user.UserResponseDto;
+import com.example.stayfinder.model.Role;
 import com.example.stayfinder.model.User;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
+    @Mapping(target = "roles", source = "roles", qualifiedByName = "rolesToStringSet")
     UserResponseDto toDto(User user);
 
     User toEntity(UserRegisterRequestDto registrationDto);
+
+    void updateEntityFromDto(@MappingTarget User user, UserRegisterRequestDto updateDto);
+
+    @Named("rolesToStringSet")
+    default Set<String> mapRolesToStrings(Set<Role> roles) {
+        return roles.stream()
+                .map(role -> role.getName().name())
+                .collect(Collectors.toSet());
+    }
 }
--- src/main/java/com/example/stayfinder/service/user/UserService.java
+++ src/main/java/com/example/stayfinder/service/user/UserService.java
@@ -3,8 +3,16 @@ package com.example.stayfinder.service.user;
 import com.example.stayfinder.dto.user.UserRegisterRequestDto;
 import com.example.stayfinder.dto.user.UserResponseDto;
 import com.example.stayfinder.exception.RegistrationException;
+import com.example.stayfinder.model.User;
+import java.util.List;
 
 public interface UserService {
     UserResponseDto register(UserRegisterRequestDto registrationDto)
             throws RegistrationException;
+
+    UserResponseDto updateUserRoles(Long userId, List<String> roleNames);
+
+    UserResponseDto findProfile(User user);
+
+    UserResponseDto updateProfile(User user, UserRegisterRequestDto updateDto);
 }
