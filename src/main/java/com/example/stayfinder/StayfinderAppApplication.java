e7ba9f3b0366a6c2b964f9b6c2e702b4ce01d21b
Serhii Shyian <serhii.shn@gmail.com>
2025-03-06 08:37:37 +0200

    initial commit

---
+++ src/main/java/com/example/stayfinder/StayfinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stayfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayfinderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayfinderAppApplication.class, args);
+    }
+
+}
